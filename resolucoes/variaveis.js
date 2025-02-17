a7e20dd1d0264e08d8fb9a9fac538bf384e9174b
gabrielrochass <grss@cin.ufpe.br>
2025-02-17 17:34:17 -0300

    feat: aula variáveis

---
+++ resolucoes/variaveis.js
@@ -0,0 +1,64 @@
+let valor1 = 10;
+let valor2 = 20;
+
+let soma = valor1 + valor2;
+let subtracao = valor1 - valor2;
+let multiplicacao = valor1 * valor2;
+let divisao = valor1 / valor2;
+let modulo = valor1 % valor2;
+
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
+let and = valor1 && valor2;
+let or = valor1 || valor2;
+let not = !valor1;
+
+console.log(and);
+console.log(or);
+console.log(not);
+
+let igual = valor1 == valor2;
+let diferente = valor1 != valor2;
+let maior = valor1 > valor2;
+let menor = valor1 < valor2;
+
+console.log(igual);
+console.log(diferente);
+console.log(maior);
+console.log(menor);
+
+let maiorIgual = valor1 >= valor2;
+let menorIgual = valor1 <= valor2;
+
+console.log(maiorIgual);
+console.log(menorIgual);
+
+let incremento = valor1++;
+let decremento = valor1--;
+
+console.log(incremento);
+console.log(decremento);
+
+console.log(typeof valor1);
+console.log(typeof valor2);
+console.log(typeof soma);
+console.log(typeof subtracao);
+console.log(typeof multiplicacao);
+console.log(typeof divisao);
+console.log(typeof modulo);
+console.log(typeof and);
+console.log(typeof or);
+console.log(typeof not);
+console.log(typeof igual);
+console.log(typeof diferente);
+console.log(typeof maior);
+console.log(typeof menor);
+console.log(typeof maiorIgual);
+console.log(typeof menorIgual);
+console.log(typeof incremento);
+console.log(typeof decremento);
