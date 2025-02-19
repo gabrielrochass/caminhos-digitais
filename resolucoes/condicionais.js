60dbe1d0307e688d7eaad00f7b563e264e69fe30
gabrielrochass <grss@cin.ufpe.br>
2025-02-19 16:38:41 -0300

    feat: resolução condicionais

---
+++ resolucoes/condicionais.js
@@ -0,0 +1,49 @@
+// problema 1
+let idade = 18;
+if (idade >= 18) {
+    console.log('Maior de idade');
+    }
+    else {
+    console.log('Menor de idade');
+    }
+
+// problema 2
+let nota = 7;
+if (nota >= 7) {
+    console.log('Aprovado');
+    }
+    else if (nota >= 5) {
+    console.log('Recuperação');
+    }
+    else {
+    console.log('Reprovado');
+    }
+
+// problema 3
+let dia = 6;
+switch (dia) {
+    case 1:
+    console.log('Domingo');
+    break;
+    case 2:
+    console.log('Segunda');
+    break;
+    case 3:
+    console.log('Terça');
+    break;
+    case 4:
+    console.log('Quarta');
+    break;
+    case 5:
+    console.log('Quinta');
+    break;
+    case 6:
+    console.log('Sexta');
+    break;
+    case 7:
+    console.log('Sábado');
+    break;
+    default:
+    console.log('Dia inválido');
+    break;
+    }
